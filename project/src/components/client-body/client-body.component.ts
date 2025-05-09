828185fc521ae87bdc404ab7192713c973471cf4
Jonathan Levy <jonathanlevy2002@gmail.com>
2025-05-08 22:05:09 -0400

    CF-9: Finished basic outline

--- project/src/components/client-body/client-body.component.ts
+++ project/src/components/client-body/client-body.component.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
 import { MatCard } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-client-body',
-  imports: [MatCard],
+  imports: [
+    MatCard,
+    MatFormFieldModule, // Import MatFormFieldModule for mat-label
+    MatInputModule,
+    MatIconModule,
+    MatListModule     // Import MatInputModule for matInput
+  ],
   templateUrl: './client-body.component.html',
   styleUrl: './client-body.component.css'
 })
--- project/src/components/navigation-header/navigation-header.component.ts
+++ project/src/components/navigation-header/navigation-header.component.ts
@@ -1,20 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common'; // Import CommonModule for async pipe
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { map, Observable, startWith } from 'rxjs';
 
 @Component({
   selector: 'app-navigation-header',
   imports: [
+    CommonModule, // Add CommonModule for async pipe
     MatFormFieldModule,
     MatInputModule,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    MatAutocompleteModule,
+    ReactiveFormsModule // Import ReactiveFormsModule for FormControl
   ],
   templateUrl: './navigation-header.component.html',
   styleUrl: './navigation-header.component.css'
 })
-export class NavigationHeaderComponent {
+export class NavigationHeaderComponent implements OnInit {
+  searchControl = new FormControl('');
+  options: string[] = ['Yeffry Bad', 'Yeffry Good', 'He goood'];
+  filteredOptions!: Observable<string[]>;
 
+  ngOnInit() {
+    this.filteredOptions = this.searchControl.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value || '')),
+    );
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.options.filter(option => option.toLowerCase().includes(filterValue));
+  }
 }
